93d43045d377c8ca9c8d6eaf6117d3012bae5fd7
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-10-24 16:34:30 +0200

    ioc

---
+++ src/Blauhaus.DeviceServices/_Ioc/RegisterIoc.cs
@@ -0,0 +1,22 @@
+﻿using Blauhaus.DeviceServices.Abstractions.Application;
+using Blauhaus.DeviceServices.Abstractions.DeviceInfo;
+using Blauhaus.DeviceServices.Abstractions.SecureStorage;
+using Blauhaus.DeviceServices.Application;
+using Blauhaus.DeviceServices.DeviceInfo;
+using Blauhaus.DeviceServices.SecureStorage;
+using Blauhaus.Ioc.Abstractions;
+
+namespace Blauhaus.DeviceServices._Ioc
+{
+    public static class RegisterIoc
+    {
+        public static IIocService RegisterBlauhausDeviceServices(this IIocService iocService)
+        {
+            iocService.RegisterImplementation<ISecureStorageService, SecureStorageService>(IocLifetime.Singleton);
+            iocService.RegisterImplementation<IDeviceInfoService, DeviceInfoService>(IocLifetime.Singleton);
+            iocService.RegisterImplementation<IApplicationInfoService, ApplicationInfoService>(IocLifetime.Singleton);
+
+            return iocService;
+        }
+    }
+}
