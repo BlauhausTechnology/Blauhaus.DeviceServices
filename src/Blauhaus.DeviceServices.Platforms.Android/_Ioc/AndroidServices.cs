ffe8a8aedecdb51f4647d8810ef68c26558578b6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-07 11:01:29 +0200

    added platform projects

---
+++ src/Blauhaus.DeviceServices.Platforms.Android/_Ioc/AndroidServices.cs
@@ -0,0 +1,13 @@
+﻿using Blauhaus.Common.ValueObjects.RuntimePlatforms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.DeviceServices.Platforms.Android._Ioc
+{
+    public class AndroidServices : ServiceCollection
+    {
+        public AndroidServices()
+        {
+            this.AddSingleton<IRuntimePlatform>(RuntimePlatform.Android);
+        }
+    }
+}
---
+++ src/Blauhaus.DeviceServices.Platforms.iOS/_Ioc/IosServices.cs
@@ -0,0 +1,13 @@
+﻿using Blauhaus.Common.ValueObjects.RuntimePlatforms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.DevicesServices.Platforms.iOS._Ioc
+{
+    public class IosServices : ServiceCollection
+    {
+        public IosServices()
+        {
+            this.AddSingleton<IRuntimePlatform>(RuntimePlatform.iOS);
+        }
+    }
+}
