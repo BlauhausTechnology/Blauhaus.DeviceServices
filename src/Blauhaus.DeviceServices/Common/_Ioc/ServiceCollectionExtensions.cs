c8fef435419bfae6dcfa004c14073e4a5fa5ce74
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-08 09:54:24 +0200

    no message

---
+++ src/Blauhaus.DeviceServices/Common/_Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Blauhaus.DeviceServices.Abstractions.Application;
+using Blauhaus.DeviceServices.Abstractions.DeviceInfo;
+using Blauhaus.DeviceServices.Abstractions.SecureStorage;
+using Blauhaus.DeviceServices.Common.Application;
+using Blauhaus.DeviceServices.Common.DeviceInfo;
+using Blauhaus.DeviceServices.Common.SecureStorage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.DeviceServices.Common._Ioc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDeviceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDeviceInfoService, DeviceInfoService>();
+            services.AddTransient<IApplicationInfoService, ApplicationInfoService>();
+            services.AddTransient<ISecureStorageService, SecureStorageService>();
+            return services;
+        }
+    }
+}
