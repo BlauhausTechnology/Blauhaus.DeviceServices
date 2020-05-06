e5e05e904dcea3b6b2818d31e6efbec8cc5a17e3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-05-06 11:26:39 +0200

    ConnectivityService

---
+++ src/Blauhaus.DeviceServices/Common/Connectivity/ConnectivityService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Blauhaus.DeviceServices.Abstractions.Connectivity;
+using Xamarin.Essentials;
+
+namespace Blauhaus.DeviceServices.Common.Connectivity
+{
+    public class ConnectivityService : IConnectivityService
+    {
+        public bool IsConnectedToInternet 
+            => Xamarin.Essentials.Connectivity.NetworkAccess == NetworkAccess.Internet;
+           
+        public bool IsConnectionUsingCellularData 
+            => Xamarin.Essentials.Connectivity.NetworkAccess == NetworkAccess.Internet 
+               && Xamarin.Essentials.Connectivity.ConnectionProfiles.Contains(ConnectionProfile.Cellular);
+    }
+}
