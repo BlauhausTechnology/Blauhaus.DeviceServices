b7a6322a4213bc784bf8cf7b1f5de58c468d1f88
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-05-07 14:47:30 +0200

    no message

--- src/Blauhaus.DeviceServices/Common/_Ioc/IocServiceExtensions.cs
+++ src/Blauhaus.DeviceServices/Common/_Ioc/IocServiceExtensions.cs
@@ -1,13 +1,12 @@
 ﻿using Blauhaus.DeviceServices.Abstractions.Application;
-using Blauhaus.DeviceServices.Abstractions.DeviceInfo;
+using Blauhaus.DeviceServices.Abstractions.Connectivity;
 using Blauhaus.DeviceServices.Abstractions.SecureStorage;
 using Blauhaus.DeviceServices.Abstractions.Thread;
 using Blauhaus.DeviceServices.Common.Application;
-using Blauhaus.DeviceServices.Common.DeviceInfo;
+using Blauhaus.DeviceServices.Common.Connectivity;
 using Blauhaus.DeviceServices.Common.SecureStorage;
 using Blauhaus.DeviceServices.Common.Thread;
 using Blauhaus.Ioc.Abstractions;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Blauhaus.DeviceServices.Common._Ioc
 {
@@ -18,6 +17,7 @@ namespace Blauhaus.DeviceServices.Common._Ioc
             iocService.RegisterImplementation<IApplicationInfoService, ApplicationInfoService>();
             iocService.RegisterImplementation<ISecureStorageService, SecureStorageService>();
             iocService.RegisterImplementation<IThreadService, ThreadService>();
+            iocService.RegisterImplementation<IConnectivityService, ConnectivityService>();
             return iocService;
         }
     }
