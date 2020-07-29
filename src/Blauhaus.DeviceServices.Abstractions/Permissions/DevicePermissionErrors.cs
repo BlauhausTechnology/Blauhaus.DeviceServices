0d4ab37ce8ac70f0827ea297d1c41adb89fb1c36
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-29 12:18:52 +0200

    no message

---
+++ src/Blauhaus.DeviceServices.Abstractions/Permissions/DevicePermissionErrors.cs
@@ -0,0 +1,13 @@
+﻿using Blauhaus.Errors;
+
+namespace Blauhaus.DeviceServices.Abstractions.Permissions
+{
+    public static class DevicePermissionErrors
+    {
+        public static Error Denied(string permissionName) => Error.Create($"Permission for {permissionName} was denied");
+        public static Error Disabled(string permissionName) => Error.Create($"Permission for {permissionName} could not be granted as the capability is disabled");
+        public static Error Restricted(string permissionName)  => Error.Create($"Permission for {permissionName} is restricted");
+        public static Error Unknown(string permissionName)  => Error.Create($"Status for {permissionName} permission could not be determined ");
+        public static Error Exception(string permissionName, string exceptionMessage)  => Error.Create($"An error occured while requesting permission for {permissionName}. The error message was {exceptionMessage}");
+    }
+}
---
+++ src/Blauhaus.DeviceServices.Abstractions/Permissions/IDevicePermissionsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+
+namespace Blauhaus.DeviceServices.Abstractions.Permissions
+{
+    public interface IDevicePermissionsService
+    {
+        Task<Result> CheckPermissionAsync(DevicePermission permission);
+        Task<Result> RequestPermissionAsync(DevicePermission permission);
+    }
+}
