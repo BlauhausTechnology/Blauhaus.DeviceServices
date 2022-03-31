3195c6b776368f21c4cc1f668e94c3ea4a0f0c4c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-03-31 17:00:26 +0200

    no message

--- src/Blauhaus.DeviceServices.Maui/Common/SecureStorageService.cs
+++ src/Blauhaus.DeviceServices.Maui/Common/SecureStorageService.cs
@@ -1,22 +1,34 @@
-﻿using Blauhaus.DeviceServices.Abstractions.SecureStorage;
+﻿using Blauhaus.Analytics.Abstractions;
+using Blauhaus.DeviceServices.Abstractions.SecureStorage;
+using Microsoft.Extensions.Logging;
 
 namespace Blauhaus.DeviceServices.Maui
 {
     public class SecureStorageService : ISecureStorageService
     {
+        private readonly IAnalyticsLogger<SecureStorageService> _logger;
+
+        public SecureStorageService(IAnalyticsLogger<SecureStorageService> logger)
+        {
+            _logger = logger;
+        }
+
         public Task<string> GetAsync(string key)
         {
-            return Microsoft.Maui.Essentials.SecureStorage.GetAsync(key);
+            _logger.LogTrace("Retrieved value for {ValueName} from secure storage", key);
+            return SecureStorage.GetAsync(key);
         }
 
         public Task SetAsync(string key, string value)
         {
-            return Microsoft.Maui.Essentials.SecureStorage.SetAsync(key, value);
+            _logger.LogTrace("Saved value for {ValueName} to secure storage", key);
+            return SecureStorage.SetAsync(key, value);
         }
 
         public bool Remove(string key)
         {
-            return Microsoft.Maui.Essentials.SecureStorage.Remove(key);
+            _logger.LogTrace("Removed value for {ValueName} from secure storage", key);
+            return SecureStorage.Remove(key);
         }
 
     }
