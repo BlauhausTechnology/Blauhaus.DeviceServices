8efdddcfa980e9cfea0a8e0999d22bd334e6a2de
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-11-09 11:52:48 +0200

    no message

---
+++ src/Blauhaus.DeviceServices.Blazor.Server/.Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Blauhaus.Common.Abstractions;
+using Blauhaus.DeviceServices.Blazor.Server.Services;
+
+namespace Blauhaus.DeviceServices.Blazor.Server.Ioc;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBlazorServerDeviceServices(this IServiceCollection services)
+    {
+        services.AddScoped<IKeyValueStore, BlazorServerKeyValueStore>();
+        return services;
+    }
+}
---
+++ src/Blauhaus.DeviceServices.Blazor.Server/Services/BlazorServerKeyValueStore.cs
@@ -0,0 +1,57 @@
+﻿using Blauhaus.Analytics.Abstractions;
+using Blauhaus.Analytics.Abstractions.Extensions;
+using Blauhaus.Common.Abstractions;
+using Blauhaus.Errors;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace Blauhaus.DeviceServices.Blazor.Server.Services;
+
+public class BlazorServerKeyValueStore : IKeyValueStore
+{
+    private readonly IAnalyticsLogger<BlazorServerKeyValueStore> _logger;
+    private readonly ProtectedLocalStorage _protectedLocalStorage;
+
+    public BlazorServerKeyValueStore(
+        IAnalyticsLogger<BlazorServerKeyValueStore> logger,
+        ProtectedLocalStorage protectedLocalStorage)
+    {
+        _logger = logger;
+        _protectedLocalStorage = protectedLocalStorage;
+    }
+
+    public async Task<string> GetAsync(string key)
+    {
+        try
+        {
+            var result = await _protectedLocalStorage.GetAsync<string>(key);
+            if (result.Success && result.Value is not null)
+            {
+                _logger.LogTrace("Retrieved value for {ValueName} from secure storage", key);
+                return result.Value;
+            }
+
+            _logger.LogInformation("Key {Key} not found in secure storage", key);
+            return string.Empty;
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(Error.Unexpected($"Failed to retried value with key {key}"), e);
+            return string.Empty;
+        }
+    }
+
+    public async Task SetAsync(string key, string value)
+    {
+        await _protectedLocalStorage.SetAsync(key, value);
+        _logger.LogTrace("Saved value for {Key} to secure storage", key);
+    }
+
+    public bool Remove(string key)
+    {
+
+        _protectedLocalStorage.DeleteAsync(key);
+        _logger.LogTrace("Removed value for {ValueName} from secure storage", (object) key);
+        return true;
+    }   
+}
