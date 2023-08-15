9cbbcf944f16b4414f94009acdf32f5e501ecce5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-08-15 16:59:49 +0200

    no message

--- src/Blauhaus.DeviceServices/Common/.Ioc/IocServiceExtensions.cs
+++ src/Blauhaus.DeviceServices/Common/.Ioc/IocServiceExtensions.cs
@@ -2,11 +2,13 @@
 using Blauhaus.DeviceServices.Abstractions.Connectivity;
 using Blauhaus.DeviceServices.Abstractions.Permissions;
 using Blauhaus.DeviceServices.Abstractions.SecureStorage;
+using Blauhaus.DeviceServices.Abstractions.Share;
 using Blauhaus.DeviceServices.Abstractions.Thread;
 using Blauhaus.DeviceServices.Common.Application;
 using Blauhaus.DeviceServices.Common.Connectivity;
 using Blauhaus.DeviceServices.Common.Permissions;
 using Blauhaus.DeviceServices.Common.SecureStorage;
+using Blauhaus.DeviceServices.Common.Share;
 using Blauhaus.DeviceServices.Common.Thread;
 using Blauhaus.Ioc.Abstractions;
 
@@ -21,6 +23,7 @@ namespace Blauhaus.DeviceServices.Common.Ioc
             iocService.RegisterImplementation<IThreadService, ThreadService>(IocLifetime.Singleton);
             iocService.RegisterImplementation<IConnectivityService, ConnectivityService>(IocLifetime.Singleton);
             iocService.RegisterImplementation<IDevicePermissionsService, DevicePermissionsService>(IocLifetime.Singleton);
+            iocService.RegisterImplementation<IShareService, ShareService>(IocLifetime.Singleton);
             return iocService;
         }
     }
