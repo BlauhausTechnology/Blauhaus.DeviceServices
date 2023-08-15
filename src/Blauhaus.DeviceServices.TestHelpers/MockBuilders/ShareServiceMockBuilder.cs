fe0bbf5c79369ca9257664cc7c3a2f743a5d6016
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-08-15 16:35:28 +0200

    no message

---
+++ src/Blauhaus.DeviceServices.TestHelpers/MockBuilders/ShareServiceMockBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Blauhaus.DeviceServices.Abstractions.Share;
+using Blauhaus.Errors;
+using Blauhaus.Responses;
+using Blauhaus.TestHelpers.MockBuilders;
+using Moq;
+
+namespace Blauhaus.DeviceServices.TestHelpers.MockBuilders;
+
+public class ShareServiceMockBuilder : BaseMockBuilder<ShareServiceMockBuilder, IShareService>
+{
+
+    public ShareServiceMockBuilder()
+    {
+        Where_ShareFileAsync_succeeds();
+    }
+
+    public ShareServiceMockBuilder Where_ShareFileAsync_succeeds()
+    {
+        Mock.Setup(x => x.ShareFileAsync(It.IsAny<byte[]>(), It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(Response.Success);
+        return this;
+    }
+
+    public Error Where_ShareFileAsync_fails(Error? err = null)
+    {
+        err??=Error.Create(nameof(Where_ShareFileAsync_fails));
+        Mock.Setup(x => x.ShareFileAsync(It.IsAny<byte[]>(), It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(Response.Failure(err.Value));
+        return err.Value;
+    }
+}
